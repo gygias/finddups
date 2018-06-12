d871d8885ac3439607eb53eac1fa587663505c4b
david <dave.desmond@me.com>
2018-06-12 14:02:28 -0700

    update map appropriately based on trashed file, reduce indentation of main block

--- finddups/main.m
+++ finddups/main.m
@@ -86,101 +86,110 @@ int main(int argc, const char * argv[]) {
         for ( NSString *aPath in fileList ) {
             if ( [aPath hasSuffix:@".DS_Store"] )
                 continue;
+            BOOL add = YES;
             NSString *fullPath = [path stringByAppendingPathComponent:aPath];
             NSDictionary *attrs = [fm attributesOfItemAtPath:fullPath error:nil];
-            if ( [attrs[NSFileType] isEqualToString:NSFileTypeRegular] ) {
-                NSNumber *size = attrs[NSFileSize];
-                NSArray *likeSized = [map allKeysForObject:size];
-                if ( [likeSized count] ) {
+            
+            if ( ! [attrs[NSFileType] isEqualToString:NSFileTypeRegular] )
+                continue;
+            
+            NSNumber *size = attrs[NSFileSize];
+            NSArray *likeSized = [map allKeysForObject:size];
+            if ( [likeSized count] ) {
+                
+                NSTask *cksum1 = [NSTask new];
+                cksum1.launchPath = @"/usr/bin/cksum";
+                cksum1.arguments = @[fullPath];
+                NSPipe *pipe1 = [NSPipe new];
+                cksum1.standardOutput = pipe1;
+                [cksum1 launch];
+                [cksum1 waitUntilExit];
+                
+                NSData *raw1 = [[pipe1 fileHandleForReading] readDataToEndOfFile];
+                if ( [cksum1 terminationStatus] != 0 ) {
+                    NSLog(@"failed to cksum %@: %d %s",aPath,[cksum1 terminationStatus],strerror([cksum1 terminationStatus]));
+                    continue;
+                }
+                
+                NSString *string1 = [NSString stringWithUTF8String:[raw1 bytes]];
+                if ( ! string1 )
+                    string1 = [NSString stringWithCString:[raw1 bytes] encoding:NSASCIIStringEncoding];
+                if ( ! string1 || ! [string1 length] ) {
+                    NSLog(@"failed to decode cksum output1 for %@ (%@)",aPath,raw1);
+                    continue;
+                }
+                
+                NSString *output1 = [[string1 componentsSeparatedByString:@" "] firstObject];
+                
+                for ( NSString *likeSize in likeSized ) {
+                    NSString *otherFullPath = [path stringByAppendingPathComponent:likeSize];
                     
-                    NSTask *cksum1 = [NSTask new];
-                    cksum1.launchPath = @"/usr/bin/cksum";
-                    cksum1.arguments = @[fullPath];
-                    NSPipe *pipe1 = [NSPipe new];
-                    cksum1.standardOutput = pipe1;
-                    [cksum1 launch];
-                    [cksum1 waitUntilExit];
+                    NSTask *cksum2 = [NSTask new];
+                    cksum2.launchPath = @"/usr/bin/cksum";
+                    cksum2.arguments = @[otherFullPath];
+                    NSPipe *pipe2 = [NSPipe new];
+                    cksum2.standardOutput = pipe2;
+                    [cksum2 launch];
+                    [cksum2 waitUntilExit];
                     
-                    NSData *raw1 = [[pipe1 fileHandleForReading] readDataToEndOfFile];
-                    if ( [cksum1 terminationStatus] != 0 ) {
-                        NSLog(@"failed to cksum %@: %d %s",aPath,[cksum1 terminationStatus],strerror([cksum1 terminationStatus]));
+                    NSData *raw2 = [[pipe2 fileHandleForReading] readDataToEndOfFile];
+                    if ( [cksum2 terminationStatus] != 0 || ! [raw2 length] ) {
+                        NSLog(@"failed to cksum like-sized %@: %d %s",likeSize,[cksum2 terminationStatus],strerror([cksum2 terminationStatus]));
                         continue;
                     }
                     
-                    NSString *string1 = [NSString stringWithUTF8String:[raw1 bytes]];
-                    if ( !string1 )
-                        string1 = [NSString stringWithCString:[raw1 bytes] encoding:NSASCIIStringEncoding];
-                    if ( !string1 ) {
-                        NSLog(@"failed to decode cksum output1 for %@ (%@)",aPath,raw1);
+                    NSString *string2 = [NSString stringWithUTF8String:[raw2 bytes]];
+                    if ( ! string2 )
+                        string2 = [NSString stringWithCString:[raw2 bytes] encoding:NSASCIIStringEncoding];
+                    if ( ! string2 || ! [string2 length] ) {
+                        NSLog(@"failed to decode cksum output2 for %@ (%@)",likeSize,raw2);
                         continue;
                     }
                     
-                    NSString *output1 = [[string1 componentsSeparatedByString:@" "] firstObject];
-                    
-                    for ( NSString *likeSize in likeSized ) {
-                        NSString *otherFullPath = [path stringByAppendingPathComponent:likeSize];
+                    NSString *output2 = [[string2 componentsSeparatedByString:@" "] firstObject];
+                    if ( [output1 isEqualToString:output2] ) {
+                        if ( verbose ) NSLog(@"*** %@ appears to be a dup of %@!",aPath,likeSize);
+                        dupSpace += [size unsignedIntegerValue];
                         
-                        NSTask *cksum2 = [NSTask new];
-                        cksum2.launchPath = @"/usr/bin/cksum";
-                        cksum2.arguments = @[otherFullPath];
-                        NSPipe *pipe2 = [NSPipe new];
-                        cksum2.standardOutput = pipe2;
-                        [cksum2 launch];
-                        [cksum2 waitUntilExit];
-                        
-                        NSData *raw2 = [[pipe2 fileHandleForReading] readDataToEndOfFile];
-                        if ( [cksum2 terminationStatus] != 0 || ! [raw2 length] ) {
-                            NSLog(@"failed to cksum like-sized %@: %d %s",likeSize,[cksum2 terminationStatus],strerror([cksum2 terminationStatus]));
-                            continue;
-                        }
-                        
-                        NSString *string2 = [NSString stringWithUTF8String:[raw2 bytes]];
-                        if ( !string2 )
-                            string2 = [NSString stringWithCString:[raw2 bytes] encoding:NSASCIIStringEncoding];
-                        if ( !string2 ) {
-                            NSLog(@"failed to decode cksum output2 for %@ (%@)",likeSize,raw2);
-                            continue;
-                        }
-                        
-                        NSString *output2 = [[string2 componentsSeparatedByString:@" "] firstObject];
-                        if ( [output1 isEqualToString:output2] ) {
-                            if ( verbose ) NSLog(@"*** %@ appears to be a dup of %@!",aPath,likeSize);
-                            dupSpace += [size unsignedIntegerValue];
+                        if ( delete ) {
+                            NSString *fileToDelete = nil;
+                            if ( shallow ) {
+                                NSInteger count1 = [[fullPath componentsSeparatedByString:@"/"] count];
+                                NSInteger count2 = [[otherFullPath componentsSeparatedByString:@"/"] count];
+                                fileToDelete = count1 > count2 ? otherFullPath : fullPath;
+                                if ( verbose ) NSLog(@"deleting shallowest %@",fileToDelete);
+                            } else {
+                                fileToDelete = fullPath;
+                                if ( verbose ) NSLog(@"deleting current file: %@",fileToDelete);
+                            }
                             
-                            if ( delete ) {
-                                NSString *fileToDelete = nil;
-                                if ( shallow ) {
-                                    NSInteger count1 = [[fullPath componentsSeparatedByString:@"/"] count];
-                                    NSInteger count2 = [[otherFullPath componentsSeparatedByString:@"/"] count];
-                                    fileToDelete = count1 > count2 ? otherFullPath : fullPath;
-                                    if ( verbose ) NSLog(@"deleting shallowest %@",fileToDelete);
+                            if ( openDelete ) {
+                                [[NSTask launchedTaskWithLaunchPath:@"/usr/bin/open" arguments:@[fileToDelete,[fileToDelete isEqualToString:fullPath] ? otherFullPath : fullPath]] waitUntilExit];
+                            } else {
+                                NSURL *url = [NSURL fileURLWithPath:fileToDelete], *outURL = nil;
+                                NSError *error = nil;
+                                BOOL okay = [fm trashItemAtURL:url resultingItemURL:&outURL error:&error];
+                                if ( ! okay ) {
+                                    NSLog(@"error trashing %@: %@",fileToDelete,error);
                                 } else {
-                                    fileToDelete = fullPath;
-                                    if ( verbose ) NSLog(@"deleting current file: %@",fileToDelete);
-                                }
-                                
-                                if ( openDelete ) {
-                                    [[NSTask launchedTaskWithLaunchPath:@"/usr/bin/open" arguments:@[fileToDelete,[fileToDelete isEqualToString:fullPath] ? otherFullPath : fullPath]] waitUntilExit];
-                                } else {
-                                    NSURL *url = [NSURL fileURLWithPath:fileToDelete], *outURL = nil;
-                                    NSError *error = nil;
-                                    BOOL okay = [fm trashItemAtURL:url resultingItemURL:&outURL error:&error];
-                                    if ( ! okay ) {
-                                        NSLog(@"error trashing %@: %@",fileToDelete,error);
-                                    } else {
-                                        if ( verbose ) NSLog(@"trashed %@",fileToDelete);
-                                    }
+                                    if ( verbose ) NSLog(@"trashed %@",fileToDelete);
                                 }
                             }
-                        } else {
-                            if ( verbose ) NSLog(@"*** %@ has the same size as %@, but they are different!",aPath,likeSize);
-                            if ( openJustSizeDups )
-                                [[NSTask launchedTaskWithLaunchPath:@"/usr/bin/open" arguments:@[fullPath,otherFullPath]] waitUntilExit];
+                            
+                            if ( [fileToDelete isEqualToString:otherFullPath] )
+                                [map removeObjectForKey:size];
+                            else
+                                add = NO;
                         }
+                    } else {
+                        if ( verbose ) NSLog(@"*** %@ has the same size as %@, but they are different!",aPath,likeSize);
+                        if ( openJustSizeDups )
+                            [[NSTask launchedTaskWithLaunchPath:@"/usr/bin/open" arguments:@[fullPath,otherFullPath]] waitUntilExit];
                     }
                 }
-                [map setObject:size forKey:aPath];
             }
+            if ( add )
+                [map setObject:size forKey:aPath];
         }
         NSLog(@"space consumed by duplicates: %lu",dupSpace);
     }
